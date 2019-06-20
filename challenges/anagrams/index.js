c490e240c703710b9ec554ea93ad1db50a5ffce4
Joey Gaitan <joeygaitan707@gmail.com>
2019-06-19 22:57:24 -0700

    adding in mocha and got the program running

---
+++ challenges/anagrams/index.js
@@ -0,0 +1,12 @@
+//Given two strings, write an algorithm to check if they are anagrams of each other. 
+//Return true if they pass the test and false if they don't. E.g
+
+//isAnagram('silent', 'listen') should return true
+
+const isAnagram = (string,otherString) => {
+    string.includes
+}
+
+module.exports = {
+    isAnagram
+}
